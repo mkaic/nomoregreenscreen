29fd950e552dd7653f0b3ad65c65471752e1869f
Kai <matthew.kai.christensen@gmail.com>
2021-04-16 23:07:54 -0700

    Patch extraction function works now.

--- train_0_2.py
+++ train_0_2.py
@@ -67,20 +67,52 @@ num_hidden_channels = 8
 #Initialize the network which will produce a coarse alpha (1 chan), confidence map (1 chan), and a number of hidden channels (num_hidden_channels chan)...
 coarse = CoarseMatteGenerator(input_channels = input_channels, output_channels = num_hidden_channels + 2, chan = 32).train().to(device)
 #... which we will then feed to the refinement network to upsample and refine the area of least confidence.
-refine = MatteRefinementNetwork(input_channels = input_channels, coarse_channels = num_hidden_channels + 2, output_channels = 1, chan = 32).train().to(device)
+refine = RefinePatches(input_channels = input_channels, coarse_channels = num_hidden_channels + 2, output_channels = 1, chan = 32).train().to(device)
 
 learning_rate = 0.00001
 
 bce_loss = nn.BCEWithLogitsLoss()
 
 #This is a simple utility function for grabbing a square patch of an image tensor of dimensions N x C x H x W.
-def get_image_patches(self, images, error_maps, patch_size = 8):
+def get_image_patches(images, error_maps, k, patch_size = 8):
 
-	patch = image[:, :, top_left_corner[0] : top_left_corner[0] + patch_size, top_left_corner[1] : top_left_corner[1] + patch_size]
+	#store the original shape
+	b, c, h, w = error_maps.shape
+	#flatten (except batch dim) the error map (this is so that topk works right)
+	err = error_maps.view(b, -1)
+	#find the highest k values in the error map for each.
+	indices = err.topk(k, dim = 1, sorted = False).indices
+	print("indices shape", indices.shape)
+	#now we make a tensor of zeros shaped like the flattened error maps
+	ref = torch.zeros_like(err)
+	#we make the values at the indices of all the top k be 1 so that we can recover those indices easily later.
+	ref.scatter_(1, indices, 1.)
+	print("ref shape", ref.shape)
+	#we reshape the 0s and 1s reference tensor back into the original shape of the error maps.
+	ref = ref.view(b,1,h,w)
+	print("reshaped ref", ref.shape)
+	#we find where the ones are, these 
+	low_confidence_idx = torch.nonzero(ref.squeeze(1))
+	print("low confidence shape", low_confidence_idx.shape)
 
-	return patches, indices
+	#we get separate lists now for the Batch coord, Y coord, and X coord of each low-confidence pixel. Total of batch_size * k in each of those lists
+	#importantly, B[###] lines up with Y[###] and X[###], so that's nice. This way we can to 
+	B, Y, X = low_confidence_idx[:,0], low_confidence_idx[:,1], low_confidence_idx[:,2]
+	print("B shape", B.shape)
 
-def replace_image_patches(self, images, patches, indices):
+	#now that we have the indices of the patches, we need to actually make the patches using .unfold()
+	#first, the channels dimension needs to be the last one because... reasons? I'm really not sure tbh why this is necessary.
+	permuted = images.permute(0,2,3,1)
+	unfolded = permuted.unfold(1, 8, 4).unfold(2, 8, 4)
+	patches = unfolded[B, Y, X]
+
+	return patches
+
+
+lol = get_image_patches(images = torch.rand(4,20,480,640, device = device), error_maps = torch.rand(4,1,480,640, device = device), k = 10000)
+print(lol.shape)
+
+def replace_image_patches(images, patches, indices):
 
 	size = patches.shape[-1]
 	image[:, :, top_left_corner[0]:top_left_corner[0]+size, top_left_corner[1]:top_left_corner[1]+size] = patch
@@ -89,7 +121,7 @@ def replace_image_patches(self, images, patches, indices):
 
 #track how many batches have been done for things like periodic outputs and eventually scheduling.
 iteration = 0
-schedule1 = 1000
+schedule1 = 2
 
 use_amp = True
 scaler = torch.cuda.amp.GradScaler(enabled = use_amp)
@@ -132,11 +164,11 @@ for epoch in range(20):
 			downsampled_input_tensor = F.interpolate(input_tensor, [input_tensor.shape[-2]//2, input_tensor.shape[-1]//2])
 			start_patch_source = torch.cat([fake_coarse_alpha, fake_coarse_foreground_residual, fake_coarse_hidden_channels, downsampled_input_tensor], 1)
 
-			start_patches = get_image_patches(start_patch_source)
-			middle_patches = get_image_patches(input_tensor)
+			start_patches = get_image_patches(start_patch_source, fake_coarse_error, 10000)
+			middle_patches = get_image_patches(input_tensor, fake_coarse_error)
 
 			#Now, feed the outputs of the coarse generator into the refinement network, which will refine patches.
-			fake_refined_alpha = refine(start_patches, middle_patches)
+			fake_refined_patches = refine(start_patches, middle_patches)
 
 			#The loss of the coarse network is the pixel difference between the real and fake coarse alphas and error maps added together.
 			coarse_loss = bce_loss(fake_coarse_alpha, real_coarse_alpha) + bce_loss(fake_coarse_error, real_coarse_error)
@@ -188,6 +220,8 @@ for epoch in range(20):
 
 
 print('\nTraining completed successfully.')
+
+"""
 	
 
 """
